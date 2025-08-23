5fcd712b34f7be1113940f7584c26484a3db879c
CM702 <redan1771@gmail.com>
2025-08-23 18:25:21 -0400

    Versión lista para Render con variables de entorno

--- app/lib/supabase.js
+++ app/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://ldvjysvwpfimelqphzdw.supabase.co';  // Tu URL
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imxkdmp5c3Z3cGZpbWVscXBoemR3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY5ODkxMTEsImV4cCI6MjA2MjU2NTExMX0.HZhWKW2sFIPa646BWtPr7zhKzvqQpKL0uWZTApP-Dx0';  // Tu public API key
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
